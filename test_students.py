7789fbb5b660d9db81e7ed8538d7ba41a1453075
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-12-24 09:39:05 +0000

    create failing test for alert_santa method

--- test_students.py
+++ test_students.py
@@ -15,6 +15,14 @@ class TestStudents(unittest.TestCase):
         # create an assert to define what we expect to be returned
         self.assertEqual(student.full_name, 'John Doe')
 
+    def test_alert_santa(self):
+        # create an instance of the Student class in order to test it
+        student = Student('John', 'Doe')
+        # with our student instance created, call the alert_santa method on it
+        student.alert_santa()
+        # test whether student.naughty_list is True
+        self.assertTrue(student.naughty_list)
+
 
 # ensure that the test suite is executed when the file is run as a script
 # without having to specify the Unittest module

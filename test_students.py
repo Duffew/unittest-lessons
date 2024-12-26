adeb19e030a05e816e0f1d07ed3ed8ef16139393
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-12-26 08:25:23 +0000

    setUp and tearDown methods

--- test_students.py
+++ test_students.py
@@ -6,28 +6,53 @@ from student import Student
 
 # set up an empty class for the test
 class TestStudents(unittest.TestCase):
+
+    # setUp the testing environment and create an instance of the entire class
+    @classmethod
+    def setUpClass(cls):
+        print("set up class")
+
+    # setUp the testing environment and create an instance of the student
+    # add print statements to see when setUp and tearDown appear within
+    # the testing sequence
+    def setUp(self):
+        print("setUp")
+        self.student = Student('John', 'Doe')
+
     
+    @classmethod
+    def tearDownClass(cls):
+        print("tear down Class")
+
+
+    def tearDown(self):
+        print("tearDown")
+
     # define a test to check a 'returns full name method'
     def test_full_name(self):
+        print("test_full_name")
         # create an instance of the Student class in order to test it
-        student = Student('John', 'Doe')
+        # removed when using setUp and tearDown
+        # student = Student('John', 'Doe')
         
         # create an assert to define what we expect to be returned
-        self.assertEqual(student.full_name, 'John Doe')
+        self.assertEqual(self.student.full_name, 'John Doe')
 
     def test_alert_santa(self):
+        print("test_alert_santa")
         # create an instance of the Student class in order to test it
-        student = Student('John', 'Doe')
+        # student = Student('John', 'Doe')
         # with our student instance created, call the alert_santa method on it
-        student.alert_santa()
+        self.student.alert_santa()
         # test whether student.naughty_list is True
-        self.assertTrue(student.naughty_list)
+        self.assertTrue(self.student.naughty_list)
 
     
     def test_email(self):
-        student = Student('John', 'Doe')
+        print("test_email")
+        # student = Student('John', 'Doe')
         
-        self.assertEqual(student.email, 'john.doe@email.com')
+        self.assertEqual(self.student.email, 'john.doe@email.com')
 
 # ensure that the test suite is executed when the file is run as a script
 # without having to specify the Unittest module
